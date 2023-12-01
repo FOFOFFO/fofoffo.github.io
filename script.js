1f71685522760e73f447b506743f7509e6407f8c
FOFOFFO <talkwithmuqing@gmail.com>
2023-11-30 18:55:39 -0800

    UPLOAD 1

---
+++ script.js
@@ -0,0 +1,24 @@
+// Define an array of audio file URLs
+const audioFiles = [
+    'audio1.mp3',
+    'audio2.mp3',
+    'audio3.mp3',
+    // Add more audio file URLs as needed
+  ];
+  
+  // Function to play a random audio
+  function playRandomAudio() {
+    // Get a random index from the array
+    const randomIndex = Math.floor(Math.random() * audioFiles.length);
+    
+    // Create an <audio> element
+    const audio = new Audio(audioFiles[randomIndex]);
+    
+    // Play the audio
+    audio.play();
+  }
+  
+  // Add click event listener to the button
+  const playButton = document.getElementById('playButton');
+  playButton.addEventListener('click', playRandomAudio);
+  
